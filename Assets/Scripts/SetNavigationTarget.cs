287b61400c8392e1fadc9effe5ce0b2595445731
Jeongwon-Ryu <jeongwon3130@gmail.com>
2024-04-29 22:17:30 +0900

    Unity

--- Assets/Scripts/SetNavigationTarget.cs
+++ Assets/Scripts/SetNavigationTarget.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class SetNavigationTarget : MonoBehaviour
 {
@@ -9,30 +10,30 @@ public class SetNavigationTarget : MonoBehaviour
     [SerializeField]
     private GameObject navTargetObject;
 
-    private NavMeshPath path; // current calculated path
+    private UnityEngine.AI.NavMeshPath path; // current calculated path
     private LineRenderer line; // linerenderer to display path
 
     private bool lineToggle = false;
 
     private void Start()
     {
-        path = new NavMeshPath();
-        line = transform.Getcomponent<LineRenderer>();
+        path = new UnityEngine.AI.NavMeshPath();
+        line = transform.GetComponent<LineRenderer>(); // create new path and get the line component
     }
 
     private void Update()
     {
         if ((Input.touchCount > 0) && (Input. GetTouch(0).phase == TouchPhase.Began))
         {
-            lineToggle = !lineToggle;
+            lineToggle = !lineToggle; // everytime touch the screen, the line toggle will be toggled
         }
-        if (lineToggle)
+        if (lineToggle) // if true, the nav mesh will calculate out part from current position
         {
-            Navmesh.CalculatePath(transform.position, navTargetObject.transform.position, Navmesh.AllAreas, path);
+            NavMesh.CalculatePath(transform.position, navTargetObject.transform.position, NavMesh.AllAreas, path);
             line.positionCount = path.corners.Length;
             line.SetPositions(path.corners);
-            line.enabled = true;
+            line.enabled = true; // nav target and configure the line renderer initially
         }
     }
    
-}]
+}
